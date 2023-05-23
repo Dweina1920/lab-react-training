7ad94b7a8642113d302e59a5bf843c1fa7b42b75
Dina <dina.yamanid@gmail.com>
2023-05-23 12:56:47 +0200

    ex-5

--- src/App.js
+++ src/App.js
@@ -42,38 +42,40 @@ function App() {
 
       <h1>Exercise 5:</h1>
 
-      <CreditCard
-        type="Visa"
-        number="0123456789018845"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="BNP"
-        owner="Maxence Bouret"
-        bgColor="#11aa99"
-        color="white"
-      />
+      <div className="flex justify-around">
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
 
-      <CreditCard
-        type="Master Card"
-        number="0123456789010995"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="N26"
-        owner="Maxence Bouret"
-        bgColor="#eeeeee"
-        color="#222222"
-      />
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
 
-      <CreditCard
-        type="Visa"
-        number="0123456789016984"
-        expirationMonth={12}
-        expirationYear={2019}
-        bank="Name of the Bank"
-        owner="Firstname Lastname"
-        bgColor="#ddbb55"
-        color="white"
-      />
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
+      </div>
     </div>
   );
 }
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,7 +1,40 @@
+import visa from '../assets/images/visa.png';
+import masterCard from '../assets/images/th (11).jpg';
 function CreditCard(props) {
+  let numbersOne = props.number.slice(0, 4);
+  let numbersTwo = props.number.slice(4, 8);
+  let numbersThree = props.number.slice(8, 12);
+  let numbersFour = props.number.slice(12, 16);
+  let numbersFinal = `${numbersOne.replace(
+    numbersOne,
+    '••••'
+  )} ${numbersTwo.replace(numbersTwo, '••••')} ${numbersThree.replace(
+    numbersThree,
+    '••••'
+  )} ${numbersFour}`;
+  //console.log(numbersFinal);
+  let dateAndBank = `Expires ${props.expirationMonth}-${props.expirationYear}  ${props.bank}`;
   return (
-    <div style={{ backgroundColor: `${props.bgColor}` } } className="w-96 m-auto my-10 h-44">
-      
+    <div
+      style={{ backgroundColor: `${props.bgColor}` }}
+      className="w-96 m-auto my-10 h-44 rounded-md"
+    >
+      <div className="flex justify-end">
+        <img
+          className="w-20 p-4 "
+          src={props.type === 'Visa' ? visa : masterCard}
+        ></img>
+      </div>
+
+      <p
+        className="text-xl  tracking-widest"
+        style={{ color: `${props.color}` }}
+      >
+        {numbersFinal}
+      </p>
+
+      <p className="flex justify-start pl-8 pt-4">{dateAndBank}</p>
+      <p className="flex justify-start pl-8">{props.owner}</p>
     </div>
   );
 }
